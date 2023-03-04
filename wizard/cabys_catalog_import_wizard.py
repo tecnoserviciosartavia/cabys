a9eebc69ce3de4e0767c8012cc2bf564fb908506
tecnoserviciosartavia <72755012+tecnoserviciosartavia@users.noreply.github.com>
2023-03-04 16:42:42 -0600

    categoria9

--- wizard/cabys_catalog_import_wizard.py
+++ wizard/cabys_catalog_import_wizard.py
@@ -120,7 +120,7 @@ class CabysCatalogImportWizard(models.TransientModel):
                 description = row[products_map['description']].value
                 code = row[products_map['code']].value
                 tax = 0.0 if row[products_map['tax']].value in ('Exento', 'na') else float(row[products_map['tax']].value[:-1]) 
-                all_products[code] = {'name': description, 'codigo': code, 'impuesto': tax, 'cabys_categoria8_id': category}
+                all_products[code] = {'name': description, 'codigo': code, 'impuesto': tax, 'cabys_categoria9_id': category}
 
             # sort categories in order to process them orderly
             order_categories = all_categories.keys()
